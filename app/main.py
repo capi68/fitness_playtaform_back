f8c833fed56a305149bceabdec35008e2f65e772
Yersi Alviarez <yersialviarez@gmail.com>
2026-02-19 20:22:36 -0500

    Creacion de security.py y modificacion de 'password_has' en POST /trainers

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from .database import engine, get_db 
 from . import models, schemas
+from .schemas import TrainersListResponse
+from .security import hash_password
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -26,7 +28,7 @@ def create_trainer(trainer: schemas.TrainerCreate, db: Session = Depends(get_db)
     db_trainer = models.Trainer(
         name=trainer.name,
         email=trainer.email,
-        password_hash=trainer.password
+        password_hash=hash_password(trainer.password)
     )
 
     db.add(db_trainer)
@@ -38,3 +40,24 @@ def create_trainer(trainer: schemas.TrainerCreate, db: Session = Depends(get_db)
 #GET Endpoint to obtain Trainers list
 #####################################
 
+@app.get("/trainers", response_model=TrainersListResponse)
+def get_trainers(
+    page: int = Query(1, ge=1),
+    size: int = Query(10, ge=1, le=100),
+    db: Session = Depends(get_db)
+):
+    total = db.query(models.Trainer).count()
+
+    Trainers = (
+        db.query(models.Trainer)
+        .offset((page - 1) * size)
+        .limit(size)
+        .all()
+    )
+
+    return {
+        "total": total,
+        "page": page,
+        "size": size,
+        "items": Trainers
+    }
